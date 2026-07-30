372caf1e835d327e67923ed74407127dc293a8de
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2026-07-30 09:51:45 +0300

    config: IoC Dependency Injection servis ve RepositoryManager kayıtları Program.cs dosyasına eklendi

---
+++ Web API/Extensions/ServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using Repository.Contrat;
+using Repository.EF_Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_API.Extensions
+{
+    public static class ServiceExtension
+    {
+       
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<RepositoryContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+        }
+
+        public static void ConfigureRepositoryManager(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        }
+    }
+}
--- Web API/Program.cs
+++ Web API/Program.cs
@@ -1,6 +1,13 @@
+using Web_API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.ConfigureRepositoryManager();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
