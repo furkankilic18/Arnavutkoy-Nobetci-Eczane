85c30d1ab817054b20e478a5d487b91f3cfc1283
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2026-07-30 11:07:29 +0300

    feat: CollectAPI veri transferi (deserialization) için gerekli DTO sınıfları eklendi

---
+++ Services/Models/CollectApiPharmacyDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Services.Models
+{
+    public class CollectApiPharmacyDto
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("dist")]
+        public string Dist { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("phone")]
+        public string Phone { get; set; }
+
+        [JsonPropertyName("loc")]
+        public string Loc { get; set; }
+    }
+}
---
+++ Services/Models/CollectApiResultDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Services.Models
+{
+    public class CollectApiResultDto
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("result")]
+        public List<CollectApiPharmacyDto> Result { get; set; }
+    }
+}
