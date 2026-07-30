e4dfcbeae1c0e7a11a8013e3414c79b29a6c1cbc
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2026-07-30 09:21:28 +0300

    feat: RepositoryContext yazıldı ve SQL Server bağlantı ayarları yapıldı

---
+++ Repository/EF Core/RepositoryContext.cs
@@ -0,0 +1,45 @@
+﻿using Entities.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.EF_Core
+{
+    public class RepositoryContext : DbContext
+    {
+
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Pharmacy> Pharmacy { get; set; }
+        public DbSet<Duty> Duties { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Örnek: İlçe tablosundaki CityId, City tablosuyla ilişkilidir (EF Core bunu genelde otomatik anlar ama açıkça yazmak iyidir)
+            modelBuilder.Entity<District>()
+                .HasOne(d => d.City)
+                .WithMany(c => c.Districts)
+                .HasForeignKey(d => d.CityId);
+
+            modelBuilder.Entity<Pharmacy>()
+                .HasOne(p => p.District)
+                .WithMany(d => d.Pharmacies)
+                .HasForeignKey(p => p.DistrictId);
+
+            modelBuilder.Entity<Duty>()
+                .HasOne(d => d.Pharmacy)
+                .WithMany(p => p.Duties)
+                .HasForeignKey(d => d.PharmacyId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
