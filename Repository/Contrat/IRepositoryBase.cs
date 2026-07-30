2a3ff0175f0dd58057fa94fa7646f2c823191df1
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2026-07-30 09:26:49 +0300

    refactor: Generic Repository base altyapısı kuruldu

---
+++ Repository/Contrat/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Repository.Contrat
+{
+    public interface IRepositoryBase<T>
+        where T : class
+    {
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+        Task CreateAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Repository/EF Core/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Contrat;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Repository.EF_Core
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+       where T : class
+    {
+        protected readonly RepositoryContext _context;
+
+        protected RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            !trackChanges ?
+                _context.Set<T>().AsNoTracking() :
+                _context.Set<T>();
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges) =>
+            !trackChanges ?
+                _context.Set<T>().Where(expression).AsNoTracking() :
+                _context.Set<T>().Where(expression);
+
+        public async Task CreateAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+    }
+}
