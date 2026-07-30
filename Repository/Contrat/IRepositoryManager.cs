58e8070049f21fe0ae35fe7be7a5954ee4a2be71
Furkan KILIÇ <162465390+furkankilic18@users.noreply.github.com>
2026-07-30 09:42:52 +0300

    feat: RepositoryManager sinifi ve merkezi veri yonetimi eklendi

---
+++ Repository/Contrat/IRepositoryManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.Contrat
+{
+    public interface IRepositoryManager
+    {
+        ICityRepository City { get; }
+        IDistrictRepository District { get; }
+        IPharmacyRepository Pharmacy { get; }
+        IDutyRepository Duty { get; }
+
+        Task SaveAsync();
+    }
+}
---
+++ Repository/EF Core/RepositoryManager.cs
@@ -0,0 +1,38 @@
+﻿using Repository.Contrat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.EF_Core
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _context;
+
+        // Lazy loading nesneleri: Sadece çağrıldıklarında hafızada (RAM) yer kaplarlar.
+        private readonly Lazy<ICityRepository> _cityRepository;
+        private readonly Lazy<IDistrictRepository> _districtRepository;
+        private readonly Lazy<IPharmacyRepository> _pharmacyRepository;
+        private readonly Lazy<IDutyRepository> _dutyRepository;
+
+        public RepositoryManager(RepositoryContext context)
+        {
+            _context = context;
+
+            _cityRepository = new Lazy<ICityRepository>(() => new CityRepository(_context));
+            _districtRepository = new Lazy<IDistrictRepository>(() => new DistrictRepository(_context));
+            _pharmacyRepository = new Lazy<IPharmacyRepository>(() => new PharmacyRepository(_context));
+            _dutyRepository = new Lazy<IDutyRepository>(() => new DutyRepository(_context));
+        }
+
+        public ICityRepository City => _cityRepository.Value;
+        public IDistrictRepository District => _districtRepository.Value;
+        public IPharmacyRepository Pharmacy => _pharmacyRepository.Value;
+        public IDutyRepository Duty => _dutyRepository.Value;
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
