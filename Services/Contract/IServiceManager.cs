9aa5c86eb36576f7050c77e54341fa7836bac634
Furkan KILIC <162465390+furkankilic18@users.noreply.github.com>
2026-07-31 09:47:27 +0300

    feat: ServiceManager sinifi ve merkezi servis yonetimi eklendi

---
+++ Services/Contract/IServiceManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Contract
+{
+    public interface IServiceManager
+    {
+        ICityService CityService { get; }
+        IDistrictService DistrictService { get; }
+        IDutyService DutyService { get; }
+        IPharmacyService PharmacyService { get; }
+    }
+}
---
+++ Services/ServiceManager.cs
@@ -0,0 +1,34 @@
+﻿using Repository.Contrat;
+using Services.Contract;
+using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<ICityService> _cityService;
+        private readonly Lazy<IDistrictService> _districtService;
+        private readonly Lazy<IDutyService> _dutyService;
+        private readonly Lazy<IPharmacyService> _pharmacyService;
+
+        public ServiceManager(IRepositoryManager repositoryManager , HttpClient httpClient)
+        {
+            _cityService = new Lazy<ICityService>(() => new CityManager(repositoryManager));
+            _districtService = new Lazy<IDistrictService>(() =>new DistrictManager(repositoryManager));
+            _dutyService = new Lazy<IDutyService>(() => new DutyManager(repositoryManager));
+            _pharmacyService = new Lazy<IPharmacyService>(() => new PharmacyManager(repositoryManager,httpClient));
+            
+        }
+        public ICityService CityService => _cityService.Value;
+
+
+        public IDistrictService DistrictService => _districtService.Value;
+
+        public IDutyService DutyService => _dutyService.Value;
+
+        public IPharmacyService PharmacyService => _pharmacyService.Value;
+    }
+}
